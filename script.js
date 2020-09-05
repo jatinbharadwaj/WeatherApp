fc27fab83c42406e208f2d1469bda0933f7c1311
jatinbharadwaj <jitinrcks@gmail.com>
2020-09-05 10:54:58 +0530

    Weather API

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-
-// API key b21dda7bff40a3bf53c832d142b56682
+//  API key b21dda7bff40a3bf53c832d142b56682
 // Endpoint:
 // - Please, use the endpoint api.openweathermap.org for your API calls
 // - Example of API call:
 // api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b21dda7bff40a3bf53c832d142b56682
-
 // Useful links:
 // - API documentation https://openweathermap.org/api
 // - Details of your plan https://openweathermap.org/price
+import $ from 'jquery'
+
 const https = require('https')
-// let inp = $('#Searchbox')
-// let btn = $('#btn')
+$(()=>{
+let inp = $('#search')
+let btn = $('#btn')
 
 let url = {
     link:'https://api.openweathermap.org/data/2.5/weather?q=',
@@ -18,7 +19,6 @@ let url = {
 }
 let state = 'delhi'
 const request = https.get(url.link+state+url.key,(response)=>{
-   
     let x = ""
     response.on('data',(data)=>{
         // console.log(data)
@@ -27,20 +27,19 @@ const request = https.get(url.link+state+url.key,(response)=>{
     response.on('end',()=>{
         console.log(x)
         let data = JSON.parse(x)
-        console.log(data.main.temp)
+        console.log(data.main.tejmp)
         console.log("done!")
-    })
-    
+    })  
 })
 
+btn.click(()=>{
+    let str = inp.val()
+    console.log('str')
 
-// btn.click(()=>{
-//     let str = inp.val()
-//     console.log(str)
-// })
+})
 
 // function toggleInputButtons(){
 //     btn.prop('disabled',inp.val()=='')
 // }
+})
 
-// inp.on('input',toggleInputButtons)
--- trial.js
+++ trial.js
@@ -1,5 +1,54 @@
 $(()=>{
-    let inp = $('#inp')
-    inp.val("Jatin")
+    const inp = $('#inp')
+    const btn = $('#btn')
+    const city = $('#city')
+    const date = $('#date')
+    const temp = $('#temp')
+    const min = $('#min')
+    let url = {
+        link:'https://api.openweathermap.org/data/2.5/weather?q=',
+        key:'&appid=b21dda7bff40a3bf53c832d142b56682'
+    }
+    //Get Current Date
+    let getDate = ()=>{
+        var d = new Date();
+
+        var month = d.getMonth()+1;
+        var day = d.getDate();
+
+        var output = d.getFullYear() + '/' +
+         ((''+month).length<2 ? '0' : '') + month + '/' +
+        ((''+day).length<2 ? '0' : '') + day;
+        return output
+    }
+    // Temperature
+    let temperature = (tmp)=>{
+        tmp = String(tmp - 273.15)
+        if(tmp.length >= 4) { tmp = tmp.substr(0, 4) } 
+        return tmp
+    }
+
+
+    btn.click(()=>{
+        let str = inp.val()
+        $.getJSON(url.link+str+url.key,(data)=>{
+            let x = data  
+            let avgTemp = temperature(x.main.temp)
+            let maxminTemp = temperature(x.main.temp_max)+ '°c' + ' / ' +  temperature(x.main.temp_min) + '°c'
+            
+
+            
+            // let min = x.main.temp
+            temp.text(avgTemp + '°c')
+            min.text(maxminTemp)
+            console.log(x)
+
+            city.text(str)
+            date.text(getDate)
+        })
+    })
+    
+
+
     
 })
